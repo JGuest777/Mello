65e758a606bef8944460aa6232d988a94e1bc9b6
James <jguest777@gmail.com>
2019-12-18 17:13:47 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+console.log('Connected');
+
+// var firstName = 'Brendan';
+// var lastName = 'Eich';
+
+// console.log('firstname: ', firstName);
+// console.log('lastname: ', lastName);
+
+// var fullName = `
+// Full Name
+
+// ---
+
+// First Name: ${firstName}
+// Last Name: ${lastName}`;
+
+// console.log(fullName);
+
+var price = 23.09;
+var tax = 0.07;
+var total = price * (1 + tax);
+// ! DOES REAL MATH! UNLIKE RUBY WHICH HAS FLOAT ETC
+console.log(total);
